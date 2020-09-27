7af3e58225c74fa167431ea84ac420401d332b3e
Dima <dimazayac@gmail.com>
2020-09-27 15:20:39 +0200

    branch for nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch('/offers.json')
+  .then((r) => {return r.json() })
+  .then (x => console.log(x))
   return (
     <div className="App">
       <header className="App-header">
