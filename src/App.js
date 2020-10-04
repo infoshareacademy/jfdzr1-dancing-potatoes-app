149f60e15812a373963fadb34aa8a50448039e67
Dima <dimazayac@gmail.com>
2020-10-04 11:19:19 +0200

    dimas commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { Button } from '@material-ui/core';
+import Nav from './Navigacja/Nav'
 
 
 function App() {
@@ -11,18 +8,7 @@ function App() {
   .then (x => console.log(x))
   return (
     <>
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" >
-          Dancing Potatoes
-          <Button variant="outlined" color='inherit'>Home</Button>
-          <Button variant="outlined" color='inherit'>Ulubione</Button>
-          <Button variant="outlined" color='inherit'>Kayak</Button>
-          <Button variant="outlined" color='inherit'>Mapa</Button>
-        </Typography>
-      </Toolbar>
-      
-    </AppBar>
+    <Nav/>
 
     </>
   );
--- src/Navigacja/Nav.js
+++ src/Navigacja/Nav.js
@@ -1,4 +1,29 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+
+
+function Nav() {
+    return (
+        <> 
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" >
+          Wiosło
+          <Button variant="outlined" color='inherit'>Home</Button>
+          <Button variant="outlined" color='inherit'>Ulubione</Button>
+          <Button variant="outlined" color='inherit'>Kayak</Button>
+          <Button variant="outlined" color='inherit'>Mapa</Button>
+        </Typography>
+      </Toolbar>
+      
+    </AppBar>
+    </>
+    ) 
+    
+}
 
 
 
