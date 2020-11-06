6e9124e42fea917c3b3856dc9eeee4421beecc6f
Dima <dimazayac@gmail.com>
2020-11-06 22:21:03 +0100

    google maps DP-58

--- src/Maps/Map.js
+++ src/Maps/Map.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import { GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import React, { useState, useEffect } from "react";
+import { GoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as offersInfo from "./offersInfo.json";
+import markerIcon from "../img/place.svg"
 
 
-class Map extends React.Component {
+function Map(){
 
-    state = {
-        offersInfo: [],
-    }
+    const [selectedPark, setSelectedPark] = useState(null);
 
+    useEffect(() => {
+      const listener = e => {
+        if (e.key === "Escape") {
+          setSelectedPark(null);
+        }
+      };
+      window.addEventListener("keydown", listener);
+  
+      return () => {
+        window.removeEventListener("keydown", listener);
+      };
+    }, []);
+  
+    return (
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{ lat: 54.352024, lng: 18.646639 }}
+        // defaultOptions={{ styles: mapStyles }}
+      >
+        {offersInfo.features.map(park => (
+          <Marker
+            key={park.properties.PARK_ID}
+            position={{
+              lat: park.geometry.coordinates[0],
+              lng: park.geometry.coordinates[1]
+            }}
+            onClick={() => {
+              setSelectedPark(park);
+            }}
+            icon={{
+              url: markerIcon,
+              scaledSize: new window.google.maps.Size(70, 50)
+            }}
+          />
+        ))}
+  
+        {selectedPark && (
+          <InfoWindow
+            onCloseClick={() => {
+              setSelectedPark(null);
+            }}
+            position={{
+              lat: selectedPark.geometry.coordinates[0],
+              lng: selectedPark.geometry.coordinates[1]
+            }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTIO}</p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    );
 
-    componentDidMount() {
-        fetch('/offers.json')
-        .then(r =>  r.json())
-        .then(data => this.setState({offersInfo: data}));       
-    }
     
-
-    render(){
-        
-        
-
-        let markersLokalization = this.state.offersInfo.map(e => {
-            return e.location
-        })
-
-        return( 
-            <GoogleMap 
-            defaultZoom={11} 
-            defaultCenter={{ lat: 54.352024, lng: 18.646639 }}
-            >
-                {markersLokalization.map((location) => {
-                    return (
-                    <>
-                        <Marker
-                        position = {{
-                            lat: parseFloat(location.lat),
-                            lng: parseFloat(location.lng) 
-                        }}
-                        onClick={() => {
-                            
-                        }}
-                        />
-
-                        
-
-                    </>
-                    )
-                    
-                })}
-
-            </GoogleMap>    
-        )
-    }
 
     
 }
