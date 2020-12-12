356ca8a969e8b8831a7b2e53d359d039db4bf26b
Kamil Jastrzembowski <jastrzembowskikamil@gmail.com>
2020-12-12 16:59:42 +0100

    Pie and Bar Chart done

--- src/Home/BarChart.js
+++ src/Home/BarChart.js
@@ -1,66 +1,100 @@
-import React, {Component} from 'react';
-import {Bar, Line, Pie} from 'react-chartjs-2';
-
-class BarChart extends Component {
-
-  constructor(props) {
-      super(props);
-      this.state = {
-          chartData : {
-            labels: ['marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec'],
-            datasets: [
-              {
-      
-                data: [
-                  15,
-                  24,
-                  46,
-                  70,
-                  130
-                ],
-                backgroundColor: 
-                ['#89C7CC',
-                '#04738D',
-                '#89C7CC',
-                '#04738D',
-                '#560F0F',
-
-                ]
-              }
-            ]
-          }
-
-      }
+import React, { Component } from "react";
+import { Bar, Line, Pie } from "react-chartjs-2";
+const DATABASE_URL = "https://dancing-app-77d2a.firebaseio.com";
+export default class BarChart extends Component {
+  state = {
+    offers: [],
+    chartData: {
+      options: {
+        yAxes: [{ ticks: { min: 0 } }],
+      },
+      labels: ["Łatwe", "Średnie", "Trudne"],
+      datasets: [
+        {
+          label: "Population",
+          data: [],
+          backgroundColor: ["#89C7CC", "#04738D", "#89C7CC"],
+        },
+      ],
+    },
+  };
+  labels = ["łatwy", "średni", "trudny"];
+  componentDidMount() {
+    fetch(`${DATABASE_URL}/offers.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        const formattedData = Object.keys(data).map((key) => ({
+          id: key,
+          ...data[key],
+        }));
+        this.setState({
+          offers: formattedData,
+        });
+      })
+      .then(this.filteringData);
   }
-    render () {
-
-      return (
-        <div className="chart"
-        style={{
-          width: "600px", 
-          height: "400px",
-          marginLeft: "100px"
-          }}>
-            <Bar 
-            data={this.state.chartData}
-  
-            options={{
-              title: {
-                display: true,
-                text:'Ilość obiektów w naszej bazie',
-                fontSize: 25
+  filteringData = () => {
+    let difficultyLevels = this.labels.map((difficulty) => {
+      return this.state.offers.filter((element) => {
+        return element.level === difficulty;
+      }).length;
+    });
+    this.setState({
+      chartData: {
+        labels: ["Łatwe", "Średnie", "Trudne"],
+        datasets: [
+          {
+            label: "Ilość obiektów",
+            data: difficultyLevels,
+            backgroundColor: ["#89C7CC", "#04738D", "#89C7CC"],
+          },
+        ],
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
               },
-              legend: {
-                display: false,
-                position: 'right'
-              }
-            }}
-            />
-        </div>
-      )
-
-    }
-
+            },
+          ],
+        },
+      },
+    });
+  };
+  render() {
+    return (
+      <div
+        className="chart"
+        style={{
+          width: "600px",
+          height: "400px",
+          marginLeft: "100px",
+        }}
+      >
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: true,
+              text: "Ilość obiektów w naszej bazie względem poziomu trudności",
+              fontSize: 20,
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+            legend: {
+              display: false,
+              position: "right",
+            },
+          }}
+        />
+      </div>
+    );
+  }
 }
-
-export default BarChart;
